532f45c56d466155409516e60a3368b194b406f6
Mikael Cavalcanti <mcs11@cin.ufpe.br>
2024-07-08 15:10:03 -0300

    feat: Add wait for animation
    Co-Authored-By: Matheus Costa <matheus.design12@gmail.com>

---
+++ Assets/Package/Runtime/Helpers/WaitForAnimationToFinish.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Notask.AnimationScheduler.Package.Runtime.Helpers
+{
+    public class WaitForAnimationToFinish : CustomYieldInstruction
+    {
+        private readonly string _stateAnimName;
+
+        private readonly Animator _animator;
+        private readonly int _layerIndex;
+
+        private AnimatorStateInfo StateInfo => _animator.GetCurrentAnimatorStateInfo(_layerIndex);
+
+        private bool CorrectAnimationIsPlaying => StateInfo.IsName(_stateAnimName);
+
+        private bool AnimationIsDone => StateInfo.normalizedTime >= 1;
+
+        public override bool keepWaiting => CorrectAnimationIsPlaying && !AnimationIsDone;
+
+
+        /// <summary>
+        /// Creates a new yield-instruction
+        /// </summary>
+        /// <param name="animator">The animator to track</param>
+        /// <param name="stateAnimName">The name of the animation</param>
+        /// <param name="layerIndex">The layer the animation is playing on</param>
+        public WaitForAnimationToFinish(Animator animator, string stateAnimName, int layerIndex = 0)
+        {
+            _animator = animator;
+            _layerIndex = layerIndex;
+            _stateAnimName = stateAnimName;
+        }
+    }
+}
